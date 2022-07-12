53516ccdb6d4a33477a1f920fb2ce7eb6d271276
Ariel <92808559+Kelbore@users.noreply.github.com>
2022-07-12 15:48:28 -0400

    "added javascript file"

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const assign = new TaskManager();
 
+document.getElementById("submit").addEventListener("click", buttonClick);
+
+
+function buttonClick() {
+  event.preventDefault();
+  let taskName = document.getElementById('name').value;
+  let description = document.getElementById('description').value;
+  let assignedTo = document.getElementById('assignedTo').value;
+  let dueDate =  document.getElementById('dueDate').value;
+  let status = document.getElementById('status').value;
+  assign.addTask(taskName, description, assignedTo, dueDate,status);
+  console.log("It Works!"); 
+}
+  //newTaskVar.addTask();
+  
+  
