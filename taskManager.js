c32008ed84b97dcc7d1ffd7339dd6ed332506bff
Ariel <92808559+Kelbore@users.noreply.github.com>
2022-07-13 18:47:41 -0400

    "update taskManager file"

--- taskManager.js
+++ taskManager.js
@@ -1,26 +1,26 @@
+const createTaskHtml = (name, assignedTo, description, status, dueDate) => {
 
-const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
-
-  const html =`
+  const html = `
    
        <div class="card w-50 list-group-item" >
         <div class="card-body">
           <h5 class="card-title">Name: ${name}</h5>
-          <p class="card-text" >Description: ${description}</p>
           <p class="card-text" >Assigned To: ${assignedTo}</p>
-          <p class="card-text" >Due Date: ${dueDate}</p>
+          <p class="card-text" >Description: ${description}</p>
           <p class="card-text" >Status: ${status}</p>
+          <p class="card-text" >Due Date: ${dueDate}</p>
           <a href="#" class="btn btn-primary">Edit</a>
           <a href="#" class="btn btn-primary">Delete</a>
+          <button type="button" class="btn btn-primary" id="done">Mark as Done</button>
         </div>
-      </div>`
-    document.querySelector('#demo').innerHTML = html;
+      </div>
+      
+`
+ return html;
 };
 
 
-const taskHtml = createTaskHtml("Final project", "on progress", "July 17th, 2022", "group 5 members", "Build an app");
 
-console.log(taskHtml);
 
 
 class TaskManager {
@@ -30,38 +30,46 @@ class TaskManager {
   }
 
 
-addTask(name, description, assignedTo, dueDate, status) { 
-  const task = {
-        id: this.currentId++,
-        name: name ,
-        description: description,
-        assignedTo: assignedTo,
-        dueDate: dueDate,
-        status: status
+  addTask(name, assignedTo, description, status, dueDate) {
+    const task = {
+      id: this.currentId++,
+      name: name,
+      assignedTo: assignedTo,
+      description: description,
+      status: status,
+      dueDate: dueDate
     };
 
-        this.tasks.push(task);
-        console.log(this.tasks);
-        this.render();
-}
-
-render() {
-  const tasksHtmlList = [];
-  for(let i=0; i<3; i++){
-    let date = new Date('July 17th, 2022');
-    let formattedDate = new Date().toString();
-    let taskHtml = createTaskHtml("final project", "on progress", formattedDate, "group 5 members", "all");
-    tasksHtmlList.push(taskHtml);
+    this.tasks.push(task);
+    console.log(this.tasks);
+    this.render();
   }
-  const taskHtml = tasksHtmlList.join('-');
-  document.getElementById('secondcard').innerHTML = taskHtml;
-  document.getElementById('thirdcard').innerHTML = taskHtml;
-  document.getElementById('fourthcard').innerHTML = taskHtml;
-  document.getElementById('fifthcard').innerHTML = taskHtml;
-}  
+
+ render() {
+    var tasksHtmlList = [];
+    var tasksHtmlVar = tasksHtmlList;
+
+    for(let i = 0; i < this.tasks.length; i++){
+      var currentTask = this.tasks[i];
+      const newDate = new Date(currentTask.dueDate);
+      // due to time zones, date is ahead by one day
+      const formattedDate = (newDate.getMonth() + 1) + '/' + newDate.getDate() + '/' + newDate.getFullYear();
+      var taskHtml = createTaskHtml(currentTask.name, 
+     currentTask.assignedTo, currentTask.description, currentTask.status, formattedDate);
+      tasksHtmlList.push(taskHtml);
+      console.log(taskHtml);
+      for(let i = 0; i < tasksHtmlList.length; i++){
+        document.getElementById("taskList").innerHTML = tasksHtmlList;
+      }
+    }
+      const tasksHtml = tasksHtmlList.join('\n');
+      const tasksList = document.querySelector('#taskList');
+      tasksList.innerHTML = tasksHtml;
+   
+  }
 }
 
-function clickMe(){
+function clickMe() {
   event.preventDefault();
   newTaskVar = new TaskManager();
   newTaskVar.addTask();
